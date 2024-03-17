9ea636509aec5ff5764d0a1d8293b39c78a05064
fireflower2012@gmail.com <fireflower2012@gmail.com>
2024-03-17 11:52:01 +0000

    Updated routing and main pages

--- CooperationHullMainSite/wwwroot/js/site.js
+++ CooperationHullMainSite/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let rootURL = window.location.origin;
+
+document.addEventListener('DOMContentLoaded', () => { pageLoadActions(); });
+
+function pageLoadActions() {
+    setNavItemActive();
+}
+
+function setNavItemActive() {
+    let pageURL = window.location.href.replace(rootURL, '');
+    $(`#topNavBar li > a[href="${pageURL}"]`).parent().addClass('active')
+}
+
+
+
