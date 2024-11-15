8865ff21fe56a606712243ef1de7ac68f19ac54a
Fireflower2012 <fireflower2012@gmail.com>
2024-11-15 18:08:05 +0000

    removed location fields from class as it's not used yet

--- CooperationHullMainSite/Services/SanityCMSCalls.cs
+++ CooperationHullMainSite/Services/SanityCMSCalls.cs
@@ -40,7 +40,6 @@ namespace CooperationHullMainSite.Services
                             date = item.date,
                             eventLink = item.eventLink,
                             imagesName = item.image,
-                            location = item.location
                         };
 
                         result.Add(homePageEvent);
