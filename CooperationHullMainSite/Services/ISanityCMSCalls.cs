6f32dca9d06d897812b54fcb80c139d1a02b2d69
Fireflower2012 <fireflower2012@gmail.com>
2024-12-17 15:02:54 +0000

    Short form blog page (#190)
    * Backend for short form blog / blog summary page
    and beginnings of styling etc.
    
    * desktop / tablet css improvements for blog summary page
    
    * Backend for short form blog / blog summary page
    and beginnings of styling etc.
    
    * desktop / tablet css improvements for blog summary page
    
    * Added page for individual plog posts and service
    Set up ability to handle custom meta data for og/ descriptions etc per page and for blog posts.
    
    * Small css updates and changed blog page name to OurBlog
    
    * Start of integrating new image handler
    
    * Added image url genration for sanity io images so we can use their transform features.
    
    * restructured blog summary page and cms calls needed to populate it.
    
    * small css updates
    
    * blog summary mobile css sort out and
    blg content page image main issues resolved.
    
    * Made css tweaks to blog page. Fixed null reference exception
    
    * Properly fixed bug with post tags. Made blog posts open in same tab
    
    ---------
    
    Co-authored-by: arthurHarding <arthurh1@hotmail.co.uk>

--- CooperationHullMainSite/Services/ISanityCMSCalls.cs
+++ CooperationHullMainSite/Services/ISanityCMSCalls.cs
@@ -1,9 +1,14 @@
 ﻿using CooperationHullMainSite.Models;
+using CooperationHullMainSite.Models.SanityCMS;
 
 namespace CooperationHullMainSite.Services
 {
     public interface ISanityCMSCalls
     {
-        Task<List<HappenginNextEvent>> GetHomePageEventsList();
+        Task<List<HappeningNextEvent>> GetHomePageEventsList();
+        Task<List<PostSummary>> GetBlogPostsList(int startIndex, int quantity);
+        Task<BlogPostContent> GetBlogPostDetails(string slug);
+        Task<PostSummary> GetLatestBlogPostSummary();
+
     }
 }
