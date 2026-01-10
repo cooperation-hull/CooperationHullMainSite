cd3629d3298a5512250ca3200d15765b0aecb33b
Fireflower2012 <fireflower2012@gmail.com>
2026-01-10 11:32:59 +0000

    Added email address to homepage signup form

--- CooperationHullMainSite/Models/HomePageModel.cs
+++ CooperationHullMainSite/Models/HomePageModel.cs
@@ -8,14 +8,4 @@
         public HomePageModel() { }
     }
 
-    //public class HappeningNextEvent
-    //{
-    //    public string title { get; set; } = "";
-    //    public string description { get; set; } = "";
-    //    public DateTime date { get; set; } 
-    //    public string imagesName { get; set; } = "";
-    //    public string imageAltText { get; set; } = "";
-    //    public string eventLink { get; set; } = "";
-    //    public string location { get; set; } = "";
-    //}
 }
